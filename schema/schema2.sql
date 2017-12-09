841050946f9134862a8b646302b0930be8a9a597
Aditya Karanjkar <aditya.karanjkar@gmail.com>
2017-12-09 17:52:30 -0500

    Schema

---
+++ schema/schema2.sql
@@ -0,0 +1,100 @@
+drop database clinicsys;
+create database clinicsys;
+
+use clinicsys;
+
+CREATE TABLE employee (
+    employeeID int not null AUTO_INCREMENT,
+    role int,
+    dob Date,
+    age int,
+    lastname varchar(25),
+    firstname varchar(25),
+    address varchar(30),
+    city varchar(10) ,
+    phone varchar(17),
+    salary double,
+    specialization varchar(20) ,
+    PRIMARY KEY (employeeID)
+)AUTO_INCREMENT=100000;
+
+
+CREATE TABLE login (
+	employeeID int not null,
+    username varchar(30) not null,
+    passwd varchar(30) not null,
+    PRIMARY KEY (employeeID),
+    FOREIGN KEY (employeeID) REFERENCES employee(employeeID)
+    
+);
+
+
+
+CREATE TABLE policy(
+    policyID int not null AUTO_INCREMENT,
+    provider varchar(25) not null,
+    startdate date,
+    enddate date,
+        
+    PRIMARY KEY (policyID)
+    #FOREIGN KEY (policyID) REFERENCES patient(policyID)
+    
+)AUTO_INCREMENT = 20170000;
+
+    
+    
+
+
+
+CREATE TABLE patient (
+    patientID int not null  AUTO_INCREMENT,
+    dob Date,
+    age int,
+    lastname varchar(25),
+    firstname varchar(25),
+    address varchar(30),
+    city varchar(10) ,
+    phone varchar(17),
+    policyID int,
+    PRIMARY KEY (patientID),
+    FOREIGN KEY (policyID) REFERENCES policy(policyID)
+    
+) AUTO_INCREMENT= 1000000;
+
+
+CREATE TABLE condition_ (
+    conditionID int not null AUTO_INCREMENT,
+    patientID int not null,
+    maindiagnosis varchar(25),
+    closed boolean,
+    
+    PRIMARY KEY (conditionID),
+    FOREIGN KEY (patientID) REFERENCES patient(patientID)
+    
+)AUTO_INCREMENT= 10000;
+
+
+
+CREATE TABLE sessions (
+    sessionID int not null AUTO_INCREMENT,
+    conditionID int not null,
+    visitdate date,
+    assessment varchar(100),
+    doctorID int,
+    nurseID int,
+    
+    symptoms varchar(50),
+    diagnosis varchar(50), 
+    recommendation varchar(50),
+    
+    height int, 
+    weight int,
+    bodytemp int,
+    pulse int,
+    
+    
+    PRIMARY KEY (sessionID,conditionID),
+    FOREIGN KEY (conditionID) REFERENCES condition_(conditionID),
+    FOREIGN KEY (doctorID) REFERENCES employee(employeeID),
+    FOREIGN KEY (nurseID) REFERENCES employee(employeeID)
+);
---
+++ schema/schema3.sql
@@ -0,0 +1,61 @@
+insert into employee(role,dob,age,lastname,firstname,address,city,phone,salary,specialization) 
+  values (1,'1980-07-21', 32,'Dsouza','Moses','298 Topi st', 'Miami','+1(321)616-2928','100000','anesthesiologist');
+  
+insert into employee(role,dob,age,lastname,firstname,address,city,phone,salary,specialization) 
+  values (1,'1985-11-01',42,'Elliot','James','227 Query st', 'Tampa','+1(381)111-2928','200000','surgeon');
+    
+insert into employee(role,dob,age,lastname,firstname,address,city,phone,salary,specialization) 
+  values (1,'1975-03-29', 46,'Bourne','Jason','876 Proxy st', 'Atlanta','+1(987)616-9999','150000','physician');
+  
+insert into employee(role,dob,age,lastname,firstname,address,city,phone,salary,specialization) 
+  values (3,'1993-01-21',24,'Cyrus','Miley','112 Hack st', 'Keywest','+1(535)000-9999','80000','physician');
+    
+insert into employee(role,dob,age,lastname,firstname,address,city,phone,salary,specialization) 
+  values (2,'1990-02-05',18,'Clone','Clone','876 Mary st', 'Atlanta','+1(767)101-1234','50000','null');
+    
+insert into login values (100000, 'moses',123456);
+insert into login values (100001, 'james',123456);
+insert into login values (100002, 'jason',123456);
+insert into login values (100003, 'miley',123456);
+insert into login values (100004, 'clone',123456);
+
+insert into policy(provider,startdate,enddate) values ('ABC', '2017-01-01','2018-01-01');
+insert into policy(provider,startdate,enddate) values ('XYZ', '2017-06-01','2018-06-01');
+insert into policy(provider,startdate,enddate) values ('MUYB', '2017-09-15','2018-09-15');
+insert into policy(provider,startdate,enddate) values ('XYZ', '2017-07-01','2018-07-01');
+
+insert into patient(dob,age,lastname,firstname,address,city,phone,policyID) values 
+('1980-07-21', 32,'Hobbs','Matt','111 Time st', 'Miami','+1(321)616-2928',20170002);
+insert into patient(dob,age,lastname,firstname,address,city,phone,policyID) values 
+('1970-11-01', 42,'Jobs','Steve','Mary star st', 'Long Beach','+1(321)616-2928',20170001);
+insert into patient(dob,age,lastname,firstname,address,city,phone,policyID) values 
+('1990-07-12', 25,'Place','Lovely','625 Stark st', 'Miami','+1(321)616-2928',20170003);
+insert into patient(dob,age,lastname,firstname,address,city,phone,policyID) values 
+('1991-06-17', 24,'','Precious','928 Youre st', 'Miami','+1(321)121-2112',20170000);
+    
+
+    
+insert into condition_ (patientID,maindiagnosis,closed) values(1000001,'Flu',true);
+insert into condition_ (patientID,maindiagnosis,closed) values(1000003,'Flu',false);
+insert into condition_ (patientID,maindiagnosis,closed) values(1000002,'Fracture',true);
+
+
+insert into sessions(conditionID,visitdate,assessment,doctorID,nurseID,symptoms,diagnosis,recommendation,height,weight,bodytemp,pulse)
+   values(10000, '2017-06-17','Patient has flu', 100000,100003,'inflammation','flu', 'tablets',123,120,102,76);
+   
+insert into sessions(conditionID,visitdate,assessment,doctorID,nurseID,symptoms,diagnosis,recommendation,height,weight,bodytemp,pulse)
+   values(10001, '2017-08-21','Patient has flu', 100001,100003,'inflammation','flu', 'tablets',101,90,104,83);
+   
+insert into sessions(conditionID,visitdate,assessment,doctorID,nurseID,symptoms,diagnosis,recommendation,height,weight,bodytemp,pulse)
+   values(10000, '2017-07-02','Patient has flu', 100000,100003,'inflammation','flu', 'injection',123,111,101,82);
+
+insert into sessions(conditionID,visitdate,assessment,doctorID,nurseID,symptoms,diagnosis,recommendation,height,weight,bodytemp,pulse)
+   values(10002, '2017-11-11','Twisted ankle', 100002,100003,'Swelling','swelling', 'tablets',101,90,104,83);
+   
+insert into sessions(conditionID,visitdate,assessment,doctorID,nurseID,symptoms,diagnosis,recommendation,height,weight,bodytemp,pulse)
+   values(10002, '2017-11-22','Twisted ankle', 100002,100003,'Swelling reduced','swelling', 'tablets',123,111,101,82);
+
+
+
+    
+    
